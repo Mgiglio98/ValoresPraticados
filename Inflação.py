a4609ed61cce65c52113e5d3f30e4206c844fcc7
Mgiglio98 <matheusgiglio6@gmail.com>
2026-07-07 16:16:53 -0300

    Update Inflação.py

--- Inflação.py
+++ Inflação.py
@@ -52,6 +52,11 @@ def carregar_base():
     df = df[df["DATACOMPRA"].notna()].copy()
     df = df[df["VALOR_NUM"].notna()].copy()
 
+    df["EMPREENDIMENTO"] = df["EMPREENDIMENTO"].astype(str).str.strip()
+    df = df[df["EMPREENDIMENTO"] != "2514"].copy()
+    
+    return df
+
     return df
 
 def classificar_grupo(codigo):
